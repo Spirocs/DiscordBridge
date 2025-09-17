e68f1e8ca164826733712dcc7b3e00e59b69575a
zastrotz <nikolaj.zastrotzky@gmx.de>
2025-09-17 13:52:20 +0200

    Added Changes

--- src/main/java/dev/spirocs/discordbridge/Main.java
+++ src/main/java/dev/spirocs/discordbridge/Main.java
@@ -1,11 +1,12 @@
 package dev.spirocs.discordbridge;
 
+import dev.spirocs.discordbridge.commands.DiscordBridgeCommand;
+import dev.spirocs.discordbridge.listener.DiscordListener;
+import dev.spirocs.discordbridge.listener.MinecraftChatListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -28,7 +29,7 @@ public class Main extends JavaPlugin {
 
         // JDA starten (asynchron)
         try {
-            jda = JDABuilder.createLight(getConfig().getString("token"), EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
+            jda = JDABuilder.createLight(getConfig().getString("discord.token"), EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
                     .build();
 
             // Listener für Discord -> Minecraft
@@ -39,6 +40,13 @@ public class Main extends JavaPlugin {
             getLogger().severe("Fehler beim Start von JDA: " + e.getMessage());
             getServer().getPluginManager().disablePlugin(this);
             return;
+        }
+
+        if (getCommand("discordbridge") != null) {
+            DiscordBridgeCommand discordBridgeCommand = new DiscordBridgeCommand(this);
+            getCommand("discordbridge").setExecutor(discordBridgeCommand);
+        } else {
+            getLogger().severe("Command 'discordbridge' fehlt in plugin.yml!");
         }
 
         // Listener für Minecraft -> Discord
@@ -81,31 +89,12 @@ public class Main extends JavaPlugin {
         return ignoreBots;
     }
 
-    private void loadConfigValues() {
+    public void loadConfigValues() {
         FileConfiguration c = getConfig();
         this.channelId = c.getString("channelId", "");
         this.guildId = c.getString("guildId", "");
         this.mcToDcFormat = c.getString("format.minecraftToDiscord", "[%player%] %message%");
         this.dcToMcFormat = c.getString("format.discordToMinecraft", "[Discord] %user%: %message%");
         this.ignoreBots = c.getBoolean("ignoreBots", true);
-    }
-
-    private void reloadPlugin() {
-        reloadConfig();
-        loadConfigValues();
-        getLogger().info("Config neu geladen.");
-    }
-
-    // /discordreload
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!cmd.getName().equalsIgnoreCase("discordreload")) return false;
-        if (!sender.hasPermission("discordbridge.reload")) {
-            sender.sendMessage("§cKeine Berechtigung.");
-            return true;
-        }
-        reloadPlugin();
-        sender.sendMessage("§aDiscordBridge-Konfig neu geladen.");
-        return true;
     }
 }
